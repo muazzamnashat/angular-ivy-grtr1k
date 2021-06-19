a57ffa01c10f2452cf80b2d16e41eeb84650b877
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-06-19 03:21:05 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { of, from } from 'rxjs';
-
+import { of, from, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -8,14 +8,22 @@ import { of, from } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   name = 'Angular ';
-
+  arr = [];
   ngOnInit(): void {
-    of(1, 2, 3).subscribe(data => console.log(data));
+    of(1, 2, 3)
+      .pipe(map(item => item * 2))
+      .subscribe(data => {
+        console.log(typeof data);
+        this.arr.push(data)});
 
-    from(['nashat', 'is', 'awesome']).subscribe(
-      next => console.log(next),
-      error => console.log('The error is', error),
-      () => console.log('It is completed')
-    );
+    from(['nashat', 'is', 'awesome'])
+      .pipe(map(item => item + '  added extra'))
+      .subscribe(
+        next => console.log(typeof next),
+        error => console.log('The error is', error),
+        () => console.log('It is completed')
+      );
+
+      console.log(this.arr)
   }
 }
