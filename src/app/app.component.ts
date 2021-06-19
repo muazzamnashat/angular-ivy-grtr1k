344e9e65f5139dd6deb14d74507cc05412fbfd59
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-06-18 21:59:08 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {of} from 'rxjs';
-
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  name = 'Angular ' 
+  name = 'Angular ';
 
   ngOnInit(): void {
-    of(1,2,3).subscribe(data => console.log('',data))
+    of(1, 2, 3).subscribe(data => console.log(data));
+
+    from(['nashat', 'is', 'awesome']).subscribe(
+      next => console.log(next),
+      error => console.log('The error is', error),
+      () => console.log('It is completed')
+    );
   }
 }
