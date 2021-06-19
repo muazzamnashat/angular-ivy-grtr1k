b0f3218b5e5b1593394f640e9345133d1d3bef39
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-06-18 21:54:16 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of} from 'rxjs';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' 
+
+  ngOnInit(): void {
+    of(1,2,3).subscribe(data => console.log(data))
+  }
 }
